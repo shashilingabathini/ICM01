6a4b17c71272a80e44f66757cffd3a747c7b49ba
shashilingabathini <shashi.lingabathini@gmail.com>
2022-12-24 14:11:52 +0530

    action context additions to api

--- ICMWidgets/WebContent/icm/custom/action/AddCase.js
+++ ICMWidgets/WebContent/icm/custom/action/AddCase.js
@@ -10,7 +10,7 @@ function(declare,Action,lang,array,Coordination) {
     return declare("icm.custom.action.AddCase" , [Action] , {
         solution : null,
         caseType : null,
-
+        canClosePage : false,
         isEnabled: function(repository, listType, items, teamspace, resultSet) {
             console.log('AddCase isEnabled is invoked');
             return true;
@@ -21,18 +21,22 @@ function(declare,Action,lang,array,Coordination) {
             window.addCaseAction  = this; // for more fine debug levels
             this.solution = this.propertiesValue.solution;
             this.caseType = this.propertiesValue.caseType;
+            this.canClosePage = this.propertiesValue.canClosePage;
+			console.log(" Properties Solution Name "+this.solution);
+			console.log(" Properties Case Type Name "+this.caseType);
+			console.log(" Properties CanClosePage  "+this.canClosePage);
             // get all the solutions and check
-            ecm.model.desktop.retrieveSolutions(lang.hitch(this,this._solutionsRetrieved(solutions)));
+            ecm.model.desktop.retrieveSolutions(lang.hitch(this,this._solutionsRetrieved));
         },
 
         _solutionsRetrieved : function(solutions) {
             console.log('_solutionsRetrieved function is invoked');
             console.dir(solutions)
             array.forEach(solutions, lang.hitch(this,function(solution) {
-                console.dir(solution);
+                //console.dir(solution);
                 if(this.solution == solution.id) {
                       this.solution = solution;
-                      this.solution.retrieveCaseType(this.caseType,lang.hitch(this,this._caseRetrieved(caseType)));
+                      this.solution.retrieveCaseType(this.caseType,lang.hitch(this,this._caseRetrieved));
                 }
             }))
         },
@@ -46,8 +50,53 @@ function(declare,Action,lang,array,Coordination) {
                     "caseType" : caseType,
                     "caseEditable" : pendingEditableCase,
                     "coordination" : new Coordination()
-                })
+                });
+                if(this.canClosePage) {
+                    //close the current page
+                    this._closeCasePage();
+                }
               }));
+            }
+        },
+        _closeCasePage  : function() {
+            // to close case page see all the activities are completed with coordination
+            var casePage = this.getActionContext("CasePage");
+            var coordination = this.getActionContext("Coordination");
+
+            if(casePage && coordination) {
+                // so before closing page check BEFORECANCEL , CANCEL , AFTERCANCEL  logics  to see any uncommited information in page widget
+                coordination[0].step(this.icmBaseConst.BEFORECANCEL, lang.hitch(this,function(results, next, skip) {
+                    console.log('checking all before cancellation');
+                    console.dir(results);
+                    console.dir(next);
+                    console.dir(skip);
+                    next(); // let's move to next step
+                }),lang.hitch(this,function(errors, next, skip) {
+                        console.dir(errors);
+                        if(error)
+                            alert('there is an error to complete BEFORECANCEL');
+                })).step(this.icmBaseConst.CANCEL,lang.hitch(this,function(results,next,skip) {
+                    console.log('checking all  cancellation');
+                    console.dir(results);
+                    console.dir(next);
+                    console.dir(skip);
+                    next(); // let's move to next step
+                }), lang.hitch(this,function(errors, next, skip) {
+                    console.dir(errors);
+                    if(error)
+                        alert('there is an error to complete CANCEL');
+                })).step(this.icmBaseConst.AFTERCANCEL,lang.hitch(this,function(results,next,skip) {
+                    console.log('checking all  AFTER CANCEL');
+                    console.dir(results);
+                    console.dir(next);
+                    console.dir(skip);
+                    next(); // let's move to next step
+                    this.broadcastEvent("icm.ClosePage")
+                }), lang.hitch(this,function(errors, next, skip) {
+                    console.dir(errors);
+                    if(error)
+                        alert('there is an error to complete CANCEL');
+                })).start();
             }
         }
 
